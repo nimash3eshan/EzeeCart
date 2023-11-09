a725541f686c3c64762b3eec836541a4437be4f6
REDTECH <rachinthanarajapaksha@gmail.com>
2023-11-09 15:50:06 +0530

    Auth service added

--- auth-service/src/main/java/org/example/Main.java
+++ auth-service/src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
 }
---
+++ auth-service/src/main/java/org/example/enums/UserRole.java
@@ -0,0 +1,5 @@
+package org.example.enums;
+
+public enum UserRole {
+    CUSTOMER,ADMIN
+}
