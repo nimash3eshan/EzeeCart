e799b7dedbaf7d9dbb09fd4eef945ee810d3168d
nimash eshan <sudusinghene.20@itfac.mrt.ac.lk>
2023-11-11 15:48:04 +0530

    CORS :|

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -3,16 +3,33 @@ package com.example.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient // Eureka Client
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
+
+    @Configuration
+    @EnableWebFlux
+    public static class CorsConfig implements WebFluxConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("*")
+                    .maxAge(3600);
+        }
+    }
+
 
 }
